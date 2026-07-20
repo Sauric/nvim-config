b646235aeb15c81bdcb527ba9c64e12f338f7834
sauric <sauric@vk.com>
2026-07-20 12:09:19 +0300

    Updates; Change colorscheme;

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,28 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    opts = {
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-      {
-        -- transparent = false,
-        theme = "wave",
-      },
-    },
-  },
-  {
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		opts = {
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+			{
+				-- transparent = false,
+				theme = "dragon",
+			},
+		},
+	},
+	{
 
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa",
-    },
-  },
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "kanagawa-dragon",
+		},
+	},
 }
